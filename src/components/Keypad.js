71601efac29b84d23e5db10741d1f71935d7099e
root <gloria.kitili@student.moringaschool.com>
2023-01-18 21:40:50 +0300

    completed event handling key and  eyes

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
 
 function Keypad (){
+function Change(){
+        
+        console.log('Entering password...')
+}
+    
     return (
-        <div></div>
+    
+    <input type = 'password' onChange = {Change}></input>
+    
     )
 }
 
